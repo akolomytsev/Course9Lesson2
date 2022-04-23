db072093e73f842bd3fba4b1287d8efc0f9c4442
akolomytsev <kolm.87@mail.ru>
2022-04-23 20:47:12 +0600

    lesson2

---
+++ src/main/java/MyLinkedList/MyLinkedList.java
@@ -0,0 +1,22 @@
+package MyLinkedList;
+
+import java.util.LinkedList;
+
+public class MyLinkedList {
+
+    static String str1 = new String("на флейте водосточных труб?\n");
+    static String str2 = new String("На чешуе жестяной рыбы\n");
+    static String str3 = new String("А вы могли бы?\n");
+    static String str4 = new String("ноктюрн сыграть\n");
+    static String str5 = new String("плеснувши краску из стакана;\n");
+    static String str6 = new String("косые скулы океана.\n");
+    static String str7 = new String("Я сразу смазал карту будня,\n");
+    static String str8 = new String("прочёл я зовы новых губ.\n");
+    static String str9 = new String("я показал на блюде студня\n");
+    static String str10 = new String("могли бы\n");
+    static String str11 = new String("А вы\n");
+    static String str12 = new String("Владимир Маяковский\n");
+
+
+
+}
